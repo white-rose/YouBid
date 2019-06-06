de2544b8345455c33b64b2919557b5743af53f54
Nathan Nguyen <nathan.nguyen@accenture.com>
2019-06-06 13:41:15 -0400

    details

--- src/App.js
+++ src/App.js
@@ -3,22 +3,34 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const numbers = ["Macbook Pro Bid", "Apple IPhoneXs Bid"];
+  const listItems = numbers.map((item) =>
+    <li>{item}</li>
+  );
+
+  function bidItem(e) {
+    e.preventDefault();
+    console.log('You want to bid for ');
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        YouBid
       </header>
+      <h1> Available Items </h1>
+      <body>
+          <p> How it works:
+          We are trying to bring more power to the consumer to be able to dictact the price.
+          The idea is real simple, you bid for the most popular items and we use our network to land that price with the consumer.
+          Once the producer accepts the bid we ship the item to your address/
+           </p>
+          <ul>{listItems}</ul>
+      </body>
+      <a href="#" onClick={bidItem}>
+        Bid
+      </a>
     </div>
   );
 }
